c8224b95ea234f2878f5d36490351fe976c76623
Agustin <agutierrez@itba.edu.ar>
2023-03-23 23:19:49 -0300

    Added simple communication. Added md5sum call.

--- aplicacion.c
+++ aplicacion.c
@@ -1,26 +1,49 @@
 #include <sys/types.h>
+#include <sys/time.h>
 #include <sys/wait.h>
+#include <sys/select.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 #define CANT_ESCLAVOS 5
+#define INITIAL_DISTRIBUTION_PERCENTAJE 0.1
 
 int main(int argc, char** argv){
 
-
+    int initialFilesPerSlave = ( (argc - 1) / CANT_ESCLAVOS ) * INITIAL_DISTRIBUTION_PERCENTAJE;
+    int currentFile = 1;
+    
     int pidSlaves[CANT_ESCLAVOS];
     int writePipesFd[CANT_ESCLAVOS];
     int readPipesFd[CANT_ESCLAVOS];
 
-    int i;
+    int maxReadFD = 0;
+
+    fd_set readfds;
+   
+
+    struct timeval interval;
+    interval.tv_sec = 10;
+    interval.tv_usec = 0;
+
+    int i, j;
     for (i = 0; i < CANT_ESCLAVOS; i++){
 
-        int pipefd[2];
-        if (pipe(pipefd) == -1){
+        int pipeFd1[2];
+        int pipeFd2[2];
+
+        if (pipe(pipeFd1) == -1 || pipe(pipeFd2) == -1){
             perror("pipe");
         }else{
-            readPipesFd[i] = pipefd[0];
-            writePipesFd[i] = pipefd[1];
+
+            writePipesFd[i] = pipeFd1[1];
+            readPipesFd[i] = pipeFd2[0];
+
+            if (readPipesFd[i] > maxReadFD){
+                maxReadFD = readPipesFd[i];
+            }
+        
         }
 
         pidSlaves[i] = fork();
@@ -28,24 +51,29 @@ int main(int argc, char** argv){
             perror("fork");
         }else if (pidSlaves[i] == 0){
             //El stdin y el stdout de los esclavos pasan a ser pipes para comunicarse con la aplicación
+            if (close(pipeFd1[1]) == -1){ 
+                perror("close");
+            }
             if (fclose(stdin) == EOF){
                 perror("fclose");
             }
-            if (dup(pipefd[0]) == -1){
+            if (dup(pipeFd1[0]) == -1){
                 perror("dup");
             }
-            if (close(pipefd[0]) == -1){
+            if (close(pipeFd1[0]) == -1){
                 perror("close");
             }
 
-
+            if (close(pipeFd2[0]) == -1){
+                perror("close");
+            }
             if (fclose(stdout) == EOF){
                 perror("fclose");
             }
-            if (dup(pipefd[1]) == -1){
+            if (dup(pipeFd2[1]) == -1){
                 perror("dup");
             }
-            if (close(pipefd[1]) == -1){
+            if (close(pipeFd2[1]) == -1){
                 perror("close");
             }
             
@@ -54,8 +82,58 @@ int main(int argc, char** argv){
 
             break;
         }
+        //Aplicacion cierra los no usados
+        if (close(pipeFd1[0]) == -1){
+            perror("close");
+        }
+        if (close(pipeFd2[1]) == -1){
+            perror("close");
+        }
+
+
+    }
+
+
+    //Distribucion inicial de archivos
+    for (i = 0; i < CANT_ESCLAVOS; i++){
+        //for (j = 0; j < initialFilesPerSlave; j++ ){
+           // char* fileName = argv[currentFile++];
+            char* fileName = "aplicacion.c";
+            write(writePipesFd[i], fileName, strlen(fileName));
+   
+        //}
+    }
+
+    char lastMd5[33];
+
+    while (1){
+        FD_ZERO(&readfds);
+        interval.tv_sec = 0;
+        interval.tv_usec = 0;   
+        for (i = 0; i < CANT_ESCLAVOS; i++){
+            FD_SET(readPipesFd[i], &readfds);
+        }
+
+        select(maxReadFD+1, &readfds, NULL, NULL, &interval);
+        
+        //Verifico quien esta listo para leer
+        int fd;
+        for (fd = 0; fd <= maxReadFD; fd++){
+            if (FD_ISSET(fd, &readfds)){
+                read(fd, lastMd5, 32);
+                lastMd5[32] = 0;
+                printf("%d :: md5 is: %s \n", fd,lastMd5);
+                //Busco a donde tengo que escribir para contestar
+                for (i = 0; i < CANT_ESCLAVOS; i++){
+                    if (readPipesFd[i] == fd){
+                        write(writePipesFd[i], "", 1);  //Terminé
+                    }
+                }
+            }
+        } 
 
     }
 
     return 0;
 }
+
--- esclavo.c
+++ esclavo.c
@@ -1,9 +1,64 @@
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/select.h>
 
-int main() {
 
 
+int main(){
+
+    fd_set dataInputFds;
+    FD_ZERO(&dataInputFds);
+    FD_SET(0, &dataInputFds);   //Preparar stdin select
+
+    struct timeval interval;
+    interval.tv_sec = 1;
+    interval.tv_usec = 0;
+
+
+    char filename[1024];
+
+    while(1){
+        select(1, &dataInputFds, NULL, NULL, &interval);
+        sleep(1);
+         
+        if (FD_ISSET(0, &dataInputFds)){
+            read(0, filename, 1024);     
+            if (filename[0] == 0){ //Un filename vacio indica que ya no hay mas archivos para procesar
+                break;
+            }
+
+            int pipefd[2];
+            if (pipe(pipefd) == -1){
+                perror("pipe");
+            }
+
+            int md5id = fork();  //Escribe al pipe
+            if (md5id == 0 ){
+                close(pipefd[0]);   //r-end
+                fclose(stdout);
+                dup(pipefd[1]);
+                close(pipefd[1]);
+                char *const params[] = {"md5sum",filename, NULL};
+                execvp("md5sum", params);
+                perror("execve");
+            }
+            close(pipefd[1]);
+
+            char md5Result[32];
+
+            waitpid(md5id, NULL, 0);  
+            read(pipefd[0], md5Result, 32);
+        
+            close(pipefd[0]);
+
+            write(1, md5Result, 32);
+          
+        }
+
+    }
 
     return 0;
+ 
 }
